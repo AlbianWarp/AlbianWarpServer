686406478bb3325a9eaf02ca55c7013a13724886
KeyboardInterrupt <KeyboardInterrupt@KeyboardInterrupt.com>
2018-09-09 14:14:02 +0200

    '/home' is now also mapped to '/'

--- warpserver/pages/home.py
+++ warpserver/pages/home.py
@@ -4,6 +4,7 @@ from flask import Blueprint, render_template
 home_page_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 
+@home_page_blueprint.route('/', methods=['GET'])
 @home_page_blueprint.route('/home', methods=['GET'])
 def home_page():
     return render_template('home.html')
