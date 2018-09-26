0497ff3306fc9050e915267b8aa7516cc58a0fc3
KeyboardInterrupt <KeyboardInterrupt@KeyboardInterrupt.com>
2018-09-26 22:26:18 +0200

    fix user model so it works with mySQL

--- warpserver/model/user.py
+++ warpserver/model/user.py
@@ -11,8 +11,8 @@ class User(BaseModel):
     password_hash = db.Column(db.String(256))
     email = db.Column(db.String(256))
     power = db.Column(db.Integer(), default=0)
-    note = db.Column(db.Text, default="")
-    locked = db.Column(db.Boolean, default=False)
+    note = db.Column(db.Text(1024), default="")
+    locked = db.Column(db.Boolean(1), default=False)
 
     def __init__(self, username, password, email):
         self.username = username
